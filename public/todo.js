bf3f5553937b84f61207f0a94c7c370ac2003ed1
Benjamin Stover <benjamin.stover@gmail.com>
2012-07-24 22:21:14 -0700

    Preserve works again. link[rel=js] works.

---
+++ public/todo.js
@@ -0,0 +1,29 @@
+// Logic for todo app.
+// By Benjamin Stover
+
+BaseWidget.event('todo:app').listen({
+  newTodo: function(e) {
+    var input = e.target.elements.input;
+    var value = input.value.replace(/^\s+|\s+$/, '');
+    if (value) {
+      var widget = new BaseWidget(['todo', 'preserve']);
+      var view = {
+        text: value,
+        id: this.children_.length + 1
+      };
+      this.addChild(widget);
+      widget.render(this.getElement(), null, view);
+    }
+    input.value = '';
+    e.preventDefault();
+    return false;
+  }
+});
+
+BaseWidget.event('todo').listen({
+  rendered: function() {
+    var parentEl = this.parent_.getElement();
+    var form = parentEl.querySelector('form');
+    parentEl.insertBefore(this.getElement(), form.nextSibling);
+  }
+});
